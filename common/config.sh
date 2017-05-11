7b4845cf2ebd2ead8fd9113064b87006d2303625
Matt Decuir <matt.decuir@govdelivery.com>
2017-05-11 11:31:17 -0500

    Update work setup instructions for fish

--- common/config.sh
+++ common/config.sh
@@ -11,13 +11,19 @@ if test -d $HOME/.nodenv; then
   eval "$(nodenv init -)"
 fi
 
-# Initialize jenv if installed
+# Initialize jenv & jenv if installed (work only)
 if test -d $HOME/.jenv; then
   eval "$(jenv init -)"
 
   export PATH="$HOME/.jenv/bin:$PATH"
-  export JAVA_HOME="$HOME/.jenv/versions/`jenv version-name`"
+  export JAVA_HOME=`jenv javahome`
 
   jenv enable-plugin export # automatically sets JAVA_HOME on directory change
   alias jenv_set_java_home='export JAVA_HOME=`jenv javahome`'
+  
+  export ORACLE_HOME=/usr/local/Cellar/instantclient-basic11/11.2.0.4.0/lib
+  export TNS_ADMIN=$ORACLE_HOME
+  export OCI_DIR=$ORACLE_HOME
+  export PATH=$ORACLE_HOME:$PATH
+  export JRUBY_OPTS="--dev -J-Xmx2048m"
 fi
--- fish/config.fish
+++ fish/config.fish
@@ -10,15 +10,19 @@ status --is-interactive; and source (rbenv init -|psub)
 # Initialize nodenv
 status --is-interactive; and source (nodenv init -|psub)
 
+# Initialize jenv & java (work only)
+if test -d ~/.jenv
+  # JAVA_HOME
+  set -x JAVA_HOME (jenv javahome)
+  alias jenv_set_java_home='export JAVA_HOME=(jenv javahome)'
+  
+  # ORACLE_HOME
+  set -x ORACLE_HOME /usr/local/Cellar/instantclient-basic11/11.2.0.4.0/lib
+  set -x TNS_ADMIN $ORACLE_HOME
+  set -x OCI_DIR $ORACLE_HOME
+  set -x PATH $PATH $ORACLE_HOME
+  set -x JRUBY_OPTS "--dev -J-Xmx2048m"
+end
+
 # Python (work only)
 # set -x PATH $PATH $HOME/Library/Python/2.7/bin
-
-# Initialize jenv (work only)
-set -x JAVA_HOME (jenv javahome)
-
-# status --is-interactive; and source (jenv init -|psub)
-# jenv enable-plugin export # automatically sets JAVA_HOME on directory change
-
-alias jenv_set_java_home='export JAVA_HOME=(jenv javahome)'
-
-# set PATH $HOME/.jenv/bin $PATH
